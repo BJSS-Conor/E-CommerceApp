977da96a0b4b696ddd4335dcf4da8e2c3a175039
BJSS-Conor <conor.odonovan2@bjss.com>
2024-12-04 17:31:53 +0000

    Added Login functionality, including unit testing.

---
+++ UserMicroService/Services/LoginService.cs
@@ -0,0 +1,34 @@
+﻿using UserMicroService.Contracts.Requests;
+using UserMicroService.Contracts.Responses;
+using UserMicroService.Secutirty;
+
+namespace UserMicroService.Services
+{
+    public class LoginService : ILoginService
+    {
+        private UserDbContext _context;
+
+        public LoginService(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        public LoginResponse Login(LoginRequest req)
+        {
+            LoginResponse res;
+            var passwordHash = PasswordEncryption.HashPassword(req.Password);
+
+            var user = _context.Users.FirstOrDefault(u => u.Username == req.Username && u.PasswordHash == passwordHash);
+            if(user == null)
+            {
+                res = new LoginResponse(false, null, "Username or Password is incorrect");                
+            } 
+            else
+            {
+                res = new LoginResponse(true, user, null);                
+            }
+
+            return res;
+        }
+    }
+}
