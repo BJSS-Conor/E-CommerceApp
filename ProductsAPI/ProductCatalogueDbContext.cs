a0c51e02e8171176058fdcbc745e6e09ebddc47d
BJSS-Conor <conor.odonovan2@bjss.com>
2024-12-05 13:02:39 +0000

    Added DbContext

---
+++ ProductsAPI/ProductCatalogueDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models;
+
+namespace ProductsAPI
+{
+    public class ProductCatalogueDbContext : DbContext
+    {
+        public ProductCatalogueDbContext(DbContextOptions<ProductCatalogueDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
